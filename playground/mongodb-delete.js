1909194b992ea657774f1a5094469ac80914e580
Voranard <pumpui1985@gmail.com>
2018-12-01 16:31:48 +0700

    Add mongodb-delete.js

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,25 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if(err){
+        console.log('Unable yo connect to MongoDb server');
+    }
+    console.log('Connected to MongoDB server');
+
+    //delete many
+    // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+    //     console.log(result);
+    // });
+    //delete one
+    // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+    //     console.log(result);
+    // });
+
+    //findOneAndDelete
+    db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+        console.log(result);
+    });
+
+    //db.close();
+});
